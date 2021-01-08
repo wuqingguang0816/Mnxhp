ec40f153e13359bd994fd86bbf812101eb323925
陈清泉 <chenqingquan@yinmaisoft.com>
2021-01-08 09:19:54 +0800

    调整下载路径

--- src/utils/define.js
+++ src/utils/define.js
@@ -3,8 +3,8 @@ const APIURl = 'http://127.0.0.1:30000'
 module.exports = {
   APIURl: APIURl,
   timeout: process.env.NODE_ENV === 'development' ? 10000 : 1000000,
-  WebSocketUrl: process.env.NODE_ENV === 'development' ? APIURl.replace('http', 'ws') + '/websocket' : process.env.VUE_APP_BASE_WSS,
-  comUploadUrl: process.env.VUE_APP_BASE_API + '/api/Common/Uploader',
+  WebSocketUrl: process.env.NODE_ENV === 'development' ? APIURl.replace('http', 'ws') + '/system/websocket' : process.env.VUE_APP_BASE_WSS,
+  comUploadUrl: process.env.VUE_APP_BASE_API + '/api/system/Common/Uploader',
   comUrl: process.env.VUE_APP_BASE_API,
   // 大屏应用前端路径
   dataV: process.env.NODE_ENV === 'development' ? 'http://localhost:8100/DataV' : process.env.VUE_APP_BASE_API + '/DataV',
