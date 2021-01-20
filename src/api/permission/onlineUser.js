6bf5975fdd4d812be255c54cd6c5c4e29247157f
chenqingquan <chenqingquan@yinmaisoft.com>
2021-01-20 19:36:59 +0800

    调整接口链接

--- src/api/permission/onlineUser.js
+++ src/api/permission/onlineUser.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 获取在线用户列表
 export const getOnlineUser = (data) => {
   return request({
-    url: '/api/Permission/Users/Online',
+    url: '/api/system/Base/OnlineUser',
     method: 'GET',
     data
   })
@@ -11,7 +11,7 @@ export const getOnlineUser = (data) => {
 // 强制下线
 export const deleteOnlineUser = (id) => {
   return request({
-    url: `/api/Permission/Users/Online/${id}`,
+    url: `/api/system/Base/OnlineUser/${id}`,
     method: 'DELETE'
   })
 }
