78bf79cf9308fb046086df334ba04fe535e938a3
陈清泉 <chenqingquan@yinmaisoft.com>
2021-01-26 22:46:45 +0800

    上传接口

--- src/views/basic/profile/components/Password.vue
+++ src/views/basic/profile/components/Password.vue
@@ -116,7 +116,7 @@ export default {
     changeImg() {
       let timestamp = Math.random()
       this.timestamp = timestamp
-      this.imgUrl = `/api/Common/ImageCode/${timestamp}`
+      this.imgUrl = `/api/file/ImageCode/${timestamp}`
     }
   }
 
