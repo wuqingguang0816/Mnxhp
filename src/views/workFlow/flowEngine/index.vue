beab51e24a4541da9205d9847f8a5f03bc6a35ee
zhm <zhuhuimin@yinmaisoft.com>
2021-05-20 16:54:30 +0800

    工作流表单预览BUG修复

--- src/views/workFlow/flowEngine/index.vue
+++ src/views/workFlow/flowEngine/index.vue
@@ -208,7 +208,7 @@ export default {
         enCode: row.enCode,
         fullName: row.fullName,
         formType: row.formType,
-        formConf: row.formData
+        flowId: row.id
       }
       this.previewVisible = true
       this.$nextTick(() => {
