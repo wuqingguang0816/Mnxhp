918acb2f0c40d09fd0562306a9f0bac40f3324e2
zhm <zhuhuimin@yinmaisoft.com>
2021-06-05 15:06:46 +0800

    版本改为3.2

--- src/utils/define.js
+++ src/utils/define.js
@@ -16,5 +16,5 @@ module.exports = {
   // 数据报表
   reportServer: process.env.NODE_ENV === 'development' ? 'http://localhost:30007' : process.env.VUE_APP_BASE_API + '/ReportServer',
   report: process.env.NODE_ENV === 'development' ? 'http://localhost:8200' : process.env.VUE_APP_BASE_API + '/Report',
-  version: '3.1.3'
+  version: '3.2'
 }
