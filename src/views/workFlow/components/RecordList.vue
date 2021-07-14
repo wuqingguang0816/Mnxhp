50d9d42d744bb4c9530dfe440c9aae60a0c55e0c
zhm <zhuhuimin@yinmaisoft.com>
2021-07-14 16:29:39 +0800

    工作流流转记录

--- src/views/workFlow/components/RecordList.vue
+++ src/views/workFlow/components/RecordList.vue
@@ -1,19 +1,15 @@
 <template>
   <el-col :span="18" style="padding-left:20px; overflow: auto;">
-    <el-timeline>
+    <el-timeline reverse>
       <template v-for="(item, i) in list">
         <el-timeline-item :timestamp="item.handleTime | toDate()" placement="top" :key="i"
           v-if="item.handleStatus==2">
-          <el-link :underline="false" type="primary" icon="icon-ym icon-ym-extend-pencil" slot="dot"
-            class="timeline-icon" />
           <el-card>
             <p class="timeline-cell">发起人：{{item.userName}}</p>
           </el-card>
         </el-timeline-item>
         <el-timeline-item :timestamp="item.handleTime | toDate()" placement="top" :key="i"
           v-else-if="item.handleStatus==3">
-          <el-link :underline="false" type="danger" icon="icon-ym icon-ym-extend-history" slot="dot"
-            class="timeline-icon" />
           <el-card>
             <p class="timeline-cell">撤回节点：{{item.nodeName}}</p>
             <p class="timeline-cell">撤回人员：{{item.userName}}</p>
@@ -22,18 +18,77 @@
         </el-timeline-item>
         <el-timeline-item :timestamp="item.handleTime | toDate()" placement="top" :key="i"
           v-else-if="item.handleStatus==4">
-          <el-link :underline="false" type="danger" icon="icon-ym icon-ym-extend-minus-circle"
-            slot="dot" class="timeline-icon" />
           <el-card>
-            <p class="timeline-cell">终止人员：{{item.userName}}</p>
+            <el-row>
+              <el-col :span='18'>
+                <p class="timeline-cell">审核节点：{{item.nodeName}}</p>
+              </el-col>
+              <el-col :span='6'>
+                <p class="timeline-cell" style="text-align:right">
+                  <el-link :underline="false" type="danger">流程终止</el-link>
+                </p>
+              </el-col>
+            </el-row>
+            <p class="timeline-cell">执行人员：{{item.userName}}</p>
+            <p class="timeline-cell">执行动作：终止</p>
             <p class="timeline-cell">终止原因：{{item.handleOpinion}}</p>
           </el-card>
         </el-timeline-item>
+        <el-timeline-item :timestamp="item.handleTime | toDate()" placement="top" :key="i"
+          v-else-if="item.handleStatus==5">
+          <el-card>
+            <el-row>
+              <el-col :span='18'>
+                <p class="timeline-cell">审核节点：{{item.nodeName}}</p>
+              </el-col>
+              <el-col :span='6'>
+                <p class="timeline-cell" style="text-align:right">
+                  <el-link :underline="false" type="primary">流程指派</el-link>
+                </p>
+              </el-col>
+            </el-row>
+            <p class="timeline-cell">执行人员：{{item.userName}}</p>
+            <p class="timeline-cell">执行动作：指派</p>
+            <p class="timeline-cell">指派人员：{{item.operatorId}}</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item :timestamp="item.handleTime | toDate()" placement="top" :key="i"
+          v-else-if="item.handleStatus==6">
+          <el-card>
+            <el-row>
+              <el-col :span='18'>
+                <p class="timeline-cell">审核节点：{{item.nodeName}}</p>
+              </el-col>
+              <el-col :span='6'>
+                <p class="timeline-cell" style="text-align:right">
+                  <el-link :underline="false" type="primary">流程加签</el-link>
+                </p>
+              </el-col>
+            </el-row>
+            <p class="timeline-cell">执行人员：{{item.userName}}</p>
+            <p class="timeline-cell">执行动作：加签</p>
+            <p class="timeline-cell">加签人员：{{item.operatorId}}</p>
+          </el-card>
+        </el-timeline-item>
+        <el-timeline-item :timestamp="item.handleTime | toDate()" placement="top" :key="i"
+          v-else-if="item.handleStatus==7">
+          <el-card>
+            <el-row>
+              <el-col :span='18'>
+                <p class="timeline-cell">审核节点：{{item.nodeName}}</p>
+              </el-col>
+              <el-col :span='6'>
+                <p class="timeline-cell" style="text-align:right">
+                  <el-link :underline="false" type="primary">流程转办</el-link>
+                </p>
+              </el-col>
+            </el-row>
+            <p class="timeline-cell">执行人员：{{item.userName}}</p>
+            <p class="timeline-cell">执行动作：转办</p>
+            <p class="timeline-cell">转办人员：{{item.operatorId}}</p>
+          </el-card>
+        </el-timeline-item>
         <el-timeline-item :timestamp="item.handleTime | toDate()" placement="top" :key="i" v-else>
-          <el-link :underline="false" type="success" icon="icon-ym icon-ym-extend-check-circle-o"
-            slot="dot" class="timeline-icon" v-if='item.handleStatus == 1' />
-          <el-link :underline="false" type="danger" icon="icon-ym icon-ym-extend-clock" slot="dot"
-            class="timeline-icon" v-else />
           <el-card>
             <el-row>
               <el-col :span='18'>
@@ -47,7 +102,7 @@
                 </p>
               </el-col>
             </el-row>
-            <p class="timeline-cell">审核人员：{{item.userName}}{{item.status?'(加签)':''}}</p>
+            <p class="timeline-cell">审核人员：{{item.userName}}{{item.status==1?'(加签)':''}}</p>
             <p class="timeline-cell" v-if="item.handleOpinion">
               审核意见：{{item.handleOpinion}}</p>
             <p class="timeline-cell" v-if="item.signImg">
@@ -56,8 +111,6 @@
         </el-timeline-item>
       </template>
       <el-timeline-item :timestamp="endTime | toDate()" placement="top" v-if='endTime'>
-        <el-link :underline="false" type="danger" icon="icon-ym icon-ym-extend-minus-circle"
-          slot="dot" class="timeline-icon" />
         <el-card>
           <p class="timeline-cell">流程结束</p>
         </el-card>
