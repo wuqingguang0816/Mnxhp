dbb358ac24ed65b9590a11d9e79b200f772d955a
zhm <zhuhuimin@yinmaisoft.com>
2021-08-18 15:25:41 +0800

    工作流代码优化

--- src/views/workFlow/workFlowForm/mixin.js
+++ src/views/workFlow/workFlowForm/mixin.js
@@ -34,6 +34,7 @@ export default {
         } else {
           this.dataForm.flowId = data.flowId
           if (this.selfInit) this.selfInit(data)
+          if (!this.billEnCode) return
           BillNumber(this.billEnCode).then(res => {
             if (data.enCode === 'crmOrder') {
               this.dataForm.orderCode = res.data
