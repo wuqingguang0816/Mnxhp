b680bc6545e9a7933bebad1921dea166ed83b38a
zhm <zhuhuimin@yinmaisoft.com>
2021-09-26 11:01:56 +0800

    打印模板去掉排序

--- src/views/system/printDev/Form.vue
+++ src/views/system/printDev/Form.vue
@@ -45,10 +45,6 @@
                   v-for="item in categoryList" />
               </el-select>
             </el-form-item>
-            <el-form-item label="排序" prop="sortCode">
-              <el-input-number :min="0" :max="9999" v-model="dataForm.sortCode"
-                controls-position="right" />
-            </el-form-item>
             <el-form-item label="模板状态" prop="enabledMark">
               <el-switch v-model="dataForm.enabledMark" :active-value="1" :inactive-value="0" />
             </el-form-item>
