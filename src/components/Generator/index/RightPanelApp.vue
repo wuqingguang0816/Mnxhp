55ed1d76219bd152828c1a9d7f7951552bbbc5a1
zhm <zhuhuimin@yinmaisoft.com>
2021-11-24 13:58:10 +0800

    表单设计器bug修复

--- src/components/Generator/index/RightPanelApp.vue
+++ src/components/Generator/index/RightPanelApp.vue
@@ -794,7 +794,7 @@ export default {
     },
     setDefaultOptions() {
       if (!this.$store.getters.hasTable) return
-      if (this.activeData.__vModel__ === undefined) return
+      if (this.activeData.__vModel__ === undefined || this.activeData.__config__.jnpfKey === 'table') return
       if (!this.activeData.__config__.tableName || this.activeData.__config__.tableName === this.mainTable) {
         let fieldOptions = this.formItemList.map(o => ({ ...o, realField: o.field }))
         this.fieldOptions = fieldOptions.filter(o => o.primaryKey != 1)
