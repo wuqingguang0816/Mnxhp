d2072efcbeebabf2163a194ce35a4ee87f198cc1
zhm <zhuhuimin@yinmaisoft.com>
2021-11-30 15:16:53 +0800

    代码优化

--- src/views/permission/role/Form.vue
+++ src/views/permission/role/Form.vue
@@ -4,18 +4,18 @@
     lock-scroll width="600px">
     <el-form ref="dataForm" :model="dataForm" :rules="dataRule" v-loading="formLoading"
       label-width="100px">
+      <el-form-item label="角色名称" prop="fullName">
+        <el-input v-model="dataForm.fullName" placeholder="输入角色名称" maxlength="50" />
+      </el-form-item>
+      <el-form-item label="角色编码" prop="enCode">
+        <el-input v-model="dataForm.enCode" placeholder="输入角色编码" />
+      </el-form-item>
       <el-form-item label="角色类型" prop="type">
         <el-select v-model="dataForm.type" placeholder="请选择角色类型">
           <el-option v-for="item in typeOptions" :key="item.enCode" :label="item.fullName"
             :value="item.enCode">
           </el-option>
         </el-select>
-      </el-form-item>
-      <el-form-item label="角色名称" prop="fullName">
-        <el-input v-model="dataForm.fullName" placeholder="输入角色名称" maxlength="50" />
-      </el-form-item>
-      <el-form-item label="角色编码" prop="enCode">
-        <el-input v-model="dataForm.enCode" placeholder="输入角色编码" />
       </el-form-item>
       <el-form-item label="排序" prop="sortCode">
         <el-input-number :min="0" :max="999999" v-model="dataForm.sortCode"
