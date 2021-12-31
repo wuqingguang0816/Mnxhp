7b625a1d17a5f6d02090c0f9550fb276413d7179
zhm <zhuhuimin@yinmaisoft.com>
2021-12-31 15:47:14 +0800

    在线开发详情子表显示合计

--- src/views/basic/dynamicModel/list/detail/Item.vue
+++ src/views/basic/dynamicModel/list/detail/Item.vue
@@ -112,7 +112,9 @@
           <div class="JNPF-common-title" v-if="item.__config__.showTitle">
             <h2>{{item.__config__.label}}</h2>
           </div>
-          <JNPF-table :data="item.__config__.defaultValue">
+          <JNPF-table :data="item.__config__.defaultValue"
+            :show-summary="!!item.__config__.defaultValue.length && item['show-summary'] && (item.summaryField && !!item.summaryField.length)"
+            :summary-method="getSummaries">
             <template v-for="(column,columnIndex) in item.__config__.children">
               <el-table-column :key="columnIndex" :prop="column.__vModel__"
                 :label="column.__config__.label" v-if="column.__config__.jnpfKey==='relationForm'">
@@ -203,6 +205,35 @@ export default {
         return item.__config__.defaultValue.join()
       }
       return item.__config__.defaultValue
+    },
+    getSummaries(param) {
+      const summaryField = this.item.summaryField
+      const { columns, data } = param;
+      const sums = [];
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = '合计';
+          return;
+        }
+        if (!summaryField.includes(column.property)) {
+          sums[index] = '';
+          return;
+        }
+        const values = data.map(item => Number(item[column.property]));
+        if (!values.every(value => isNaN(value))) {
+          sums[index] = values.reduce((prev, curr) => {
+            const value = Number(curr);
+            if (!isNaN(value)) {
+              return prev + curr;
+            } else {
+              return prev;
+            }
+          }, 0);
+        } else {
+          sums[index] = '';
+        }
+      });
+      return sums
     }
   }
 }
