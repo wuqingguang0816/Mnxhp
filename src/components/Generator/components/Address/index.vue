f25135f7e8211122e0c9166ddac2888910e5cb49
zhm <zhuhuimin@yinmaisoft.com>
2022-01-18 15:37:23 +0800

    省市区组件优化

--- src/components/Generator/components/Address/index.vue
+++ src/components/Generator/components/Address/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="popupSelect-container">
-    <div class="popupSelect-input" @click="openDialog">
-      <el-input :placeholder="placeholder" v-model="innerValue" readonly :disabled="disabled">
-        <i slot="suffix" class="el-input__icon el-icon-circle-close" @click.stop="clear"
-          v-if="clearable && !disabled"></i>
-        <i slot="suffix" class="el-input__icon el-icon-arrow-down"
-          :class="{'clearable':clearable && !disabled}"></i>
+    <div class="el-select" @click.stop="openDialog">
+      <div class="el-select__tags" v-if="multiple" ref="tags"
+        :style="{ 'max-width': inputWidth - 32 + 'px', width: '100%',cursor:'pointer' }">
+        <span v-if="collapseTags && tagsList.length">
+          <el-tag :closable="!selectDisabled" :size="collapseTagSize" type="info"
+            @close="deleteTag($event, 0)" disable-transitions>
+            <span class="el-select__tags-text">{{ tagsList[0] }}</span>
+          </el-tag>
+          <el-tag v-if="tagsList.length > 1" :closable="false" type="info" disable-transitions>
+            <span class="el-select__tags-text">+ {{ tagsList.length - 1 }}</span>
+          </el-tag>
+        </span>
+        <transition-group @after-leave="resetInputHeight" v-if="!collapseTags">
+          <el-tag v-for="(item,i) in tagsList" :key="item" :size="collapseTagSize"
+            :closable="!selectDisabled" type="info" @close="deleteTag($event, i)"
+            disable-transitions>
+            <span class="el-select__tags-text">{{ item }}</span>
+          </el-tag>
+        </transition-group>
+      </div>
+      <el-input ref="reference" v-model="innerValue" type="text" :placeholder="currentPlaceholder"
+        :disabled="selectDisabled" readonly :validate-event="false"
+        :tabindex="(multiple) ? '-1' : null" @mouseenter.native="inputHovering = true"
+        @mouseleave.native="inputHovering = false">
+        <template slot="suffix">
+          <i v-show="!showClose"
+            :class="['el-select__caret', 'el-input__icon', 'el-icon-arrow-up']"></i>
+          <i v-if="showClose" class="el-select__caret el-input__icon el-icon-circle-close"
+            @click="handleClearClick"></i>
+        </template>
       </el-input>
     </div>
     <el-dialog title="省市区" :close-on-click-modal="false" :visible.sync="visible"
@@ -52,11 +76,20 @@
 
 <script>
 import { getProvinceSelector, GetAreaByIds } from '@/api/system/province'
+import { addResizeListener, removeResizeListener } from 'element-ui/src/utils/resize-event';
 export default {
   name: 'JNPF-Address',
+  inject: {
+    elForm: {
+      default: ''
+    },
+    elFormItem: {
+      default: ''
+    }
+  },
   props: {
     value: {
-      default: []
+      default: () => []
     },
     level: {
       type: Number,
@@ -74,10 +107,15 @@ export default {
       type: Boolean,
       default: false
     },
+    collapseTags: {
+      type: Boolean,
+      default: false
+    },
     clearable: {
       type: Boolean,
       default: true
-    }
+    },
+    size: String,
   },
   data() {
     return {
@@ -93,31 +131,99 @@ export default {
       },
       selectedData: [],
       selectedIds: [],
+      tagsList: [],
+      inputHovering: false,
+      inputWidth: 0,
+      initialInputHeight: 0,
     }
   },
+  computed: {
+    showClose() {
+      let hasValue = Array.isArray(this.value) && this.value.length > 0
+      let criteria = this.clearable &&
+        !this.selectDisabled &&
+        this.inputHovering &&
+        hasValue;
+      return criteria;
+    },
+    currentPlaceholder() {
+      if (this.multiple && Array.isArray(this.value) && this.value.length) {
+        return ''
+      } else {
+        return this.placeholder
+      }
+    },
+    selectDisabled() {
+      return this.disabled || (this.elForm || {}).disabled;
+    },
+    _elFormItemSize() {
+      return (this.elFormItem || {}).elFormItemSize;
+    },
+    selectSize() {
+      return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
+    },
+    collapseTagSize() {
+      return ['small', 'mini'].indexOf(this.selectSize) > -1
+        ? 'mini'
+        : 'small';
+    },
+  },
   created() {
+    this.nodeId = '-1'
+    this.initData()
+  },
+  mounted() {
+    addResizeListener(this.$el, this.handleResize);
+
+    const reference = this.$refs.reference;
+    if (reference && reference.$el) {
+      const sizeMap = {
+        medium: 36,
+        small: 32,
+        mini: 28
+      };
+      const input = reference.$el.querySelector('input');
+      this.initialInputHeight = input.getBoundingClientRect().height || sizeMap[this.selectSize];
+    }
+    if (this.multiple) {
+      this.resetInputHeight();
+    }
+    this.$nextTick(() => {
+      if (reference && reference.$el) {
+        this.inputWidth = reference.$el.getBoundingClientRect().width;
+      }
+    });
     this.setDefault()
+  },
+  beforeDestroy() {
+    if (this.$el && this.handleResize) removeResizeListener(this.$el, this.handleResize);
   },
   watch: {
     value(val) {
       this.setDefault()
-    }
+    },
+    selectDisabled() {
+      this.$nextTick(() => {
+        this.resetInputHeight();
+      });
+    },
   },
   methods: {
     onClose() { },
     clear() {
-      if (this.disabled) return
+      if (this.selectDisabled) return
       this.innerValue = ''
       this.selectedData = []
       this.selectedIds = []
+      this.tagsList = []
       this.$emit('input', [])
-      this.$emit('change', [], null)
+      this.$emit('change', [], [])
     },
     openDialog() {
-      if (this.disabled) return
+      if (this.selectDisabled) return
       this.visible = true
       this.nodeId = '-1'
-      this.initData()
+
     },
     loadNode(node, resolve) {
       if (node.level === 0) {
@@ -176,7 +282,6 @@ export default {
       this.selectedIds.splice(index, 1)
     },
     confirm() {
-      this.innerValue = this.selectedData.join(',')
       let selectedData = []
       for (let i = 0; i < this.selectedIds.length; i++) {
         let item = []
@@ -190,9 +295,12 @@ export default {
         selectedData.push(item)
       }
       if (this.multiple) {
+        this.innerValue = ''
+        this.tagsList = JSON.parse(JSON.stringify(this.selectedData))
         this.$emit('input', this.selectedIds)
         this.$emit('change', this.selectedIds, selectedData)
       } else {
+        this.innerValue = this.selectedData.join(',')
         this.$emit('input', this.selectedIds[0])
         this.$emit('change', this.selectedIds[0], selectedData[0])
       }
@@ -209,10 +317,63 @@ export default {
       GetAreaByIds(this.selectedIds).then(res => {
         if (!this.selectedData.length && !this.innerValue) {
           this.selectedData = res.data.map(o => o.join(' / '))
-          this.innerValue = this.selectedData.join(',')
+          if (this.multiple) {
+            this.innerValue = ''
+            this.tagsList = JSON.parse(JSON.stringify(this.selectedData))
+          } else {
+            this.innerValue = this.selectedData.join(',')
+          }
         }
+        this.$nextTick(() => {
+          if (this.multiple) {
+            this.resetInputHeight();
+          }
+        });
       })
     },
+    deleteTag(event, index) {
+      this.selectedData.splice(index, 1)
+      this.selectedIds.splice(index, 1)
+      this.confirm()
+      event.stopPropagation();
+    },
+    handleClearClick(event) {
+      this.selectedData = []
+      this.selectedIds = []
+      this.confirm()
+      event.stopPropagation();
+    },
+    resetInputWidth() {
+      this.inputWidth = this.$refs.reference.$el.getBoundingClientRect().width;
+    },
+    handleResize() {
+      this.resetInputWidth();
+      if (this.multiple) this.resetInputHeight();
+    },
+    resetInputHeight() {
+      if (this.collapseTags) return;
+      this.$nextTick(() => {
+        if (!this.$refs.reference) return;
+        let inputChildNodes = this.$refs.reference.$el.childNodes;
+        let input = [].filter.call(inputChildNodes, item => item.tagName === 'INPUT')[0];
+        const tags = this.$refs.tags;
+        const tagsHeight = tags ? Math.round(tags.getBoundingClientRect().height) : 0;
+        const sizeInMap = this.initialInputHeight || 40;
+        input.style.height = this.selectedData.length === 0
+          ? sizeInMap + 'px'
+          : Math.max(
+            tags ? (tagsHeight + (tagsHeight > sizeInMap ? 6 : 0)) : 0,
+            sizeInMap
+          ) + 'px';
+      });
+    },
+    resetInputWidth() {
+      this.inputWidth = this.$refs.reference.$el.getBoundingClientRect().width;
+    },
+    handleResize() {
+      this.resetInputWidth();
+      if (this.multiple) this.resetInputHeight();
+    }
   }
 }
 </script>
