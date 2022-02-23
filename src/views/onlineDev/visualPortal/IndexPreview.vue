4cddc20c666b0bdcb8fe111c7437aab88cfcc37f
zhm <zhuhuimin@yinmaisoft.com>
2022-02-23 14:00:26 +0800

    门户设计 增加一个绑定url方式入口

--- src/views/onlineDev/visualPortal/IndexPreview.vue
+++ src/views/onlineDev/visualPortal/IndexPreview.vue
@@ -13,7 +13,11 @@
     </div>
     <div class="main" style="padding:0" v-loading="loading"
       :element-loading-text="$t('common.loadingText')">
-      <PortalLayout :layout="layout" mask />
+      <div class="custom-page" v-if="type===1">
+        <component :is="currentView" v-if="linkType===0" />
+        <embed :src="url" width="100%" height="100%" type="text/html" v-if="linkType===1" />
+      </div>
+      <PortalLayout :layout="layout" mask v-if="type===0" />
     </div>
   </el-dialog>
 </template>
@@ -27,6 +31,10 @@ export default {
   data() {
     return {
       layout: [],
+      type: null,
+      linkType: null,
+      currentView: null,
+      url: '',
       loading: false
     }
   },
@@ -35,7 +43,15 @@ export default {
       this.loading = true
       this.layout = []
       getPortalInfo(this.id).then(res => {
-        if (res.data && res.data.formData) {
+        this.type = res.data.type || 0
+        this.linkType = res.data.linkType || 0
+        this.url = res.data.customUrl
+        if (!res.data) return this.loading = false
+        if (res.data.type === 1) {
+          if (!res.data.customUrl && this.linkType === 1) return
+          this.currentView = (resolve) => require([`@/views/${res.data.customUrl}`], resolve)
+        } else {
+          if (!res.data.formData) return
           let formData = JSON.parse(res.data.formData)
           this.layout = formData.layout || []
         }
@@ -48,3 +64,10 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.custom-page {
+  padding: 10px;
+  height: 100%;
+  width: 100%;
+}
+</style>
--- src/views/onlineDev/visualPortal/index.vue
+++ src/views/onlineDev/visualPortal/index.vue
@@ -30,7 +30,7 @@
       </el-row>
       <div class="JNPF-common-layout-main JNPF-flex-main">
         <div class="JNPF-common-head">
-          <topOpts @add="addOrUpdateHandle()" addText="新建门户">
+          <topOpts @add="dialogVisible=true" addText="新建门户">
             <upload-btn url="/api/visualdev/Portal/Model/Actions/ImportData"
               @on-success="initData" />
           </topOpts>
@@ -59,7 +59,8 @@
           </el-table-column>
           <el-table-column label="操作" fixed="right" width="150">
             <template slot-scope="scope">
-              <tableOpts @edit="addOrUpdateHandle(scope.row.id)" @del="handleDel(scope.row.id)">
+              <tableOpts @edit="addOrUpdateHandle(scope.row.type,scope.row.id)"
+                @del="handleDel(scope.row.id)">
                 <el-dropdown>
                   <span class="el-dropdown-link">
                     <el-button type="text" size="mini">{{$t('common.moreBtn')}}<i
@@ -82,20 +83,41 @@
           :limit.sync="listQuery.pageSize" @pagination="initData" />
       </div>
     </div>
-    <Form v-if="formVisible" ref="Form" @close="closeForm" />
+    <Form v-if="formVisible" ref="form" @close="closeForm" />
+    <Form1 v-if="form1Visible" ref="form1" @close="closeForm1" />
     <Preview :visible.sync="previewVisible" :id="activeId" />
     <Transfer ref="transfer" :visible.sync="transferShow" :id="transferId" />
+    <el-dialog title="新建门户" :visible.sync="dialogVisible"
+      class="JNPF-dialog JNPF-dialog-add JNPF-dialog_center" lock-scroll width="600px">
+      <div class="add-main">
+        <div class="add-item add-item-sys" @click="addOrUpdateHandle(1)">
+          <i class="add-icon icon-ym icon-ym-customUrl"></i>
+          <div class="add-txt">
+            <p class="add-title">自定义路径</p>
+            <p class="add-desc">配置静态页面地址</p>
+          </div>
+        </div>
+        <div class="add-item" @click="addOrUpdateHandle(0)">
+          <i class="add-icon icon-ym icon-ym-pageDesign"></i>
+          <div class="add-txt">
+            <p class="add-title">页面设计</p>
+            <p class="add-desc">拖拽生成门户</p>
+          </div>
+        </div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { getPortalList, Delete, Copy, exportTemplate } from '@/api/onlineDev/portal'
 import Form from './Form'
+import Form1 from './Form1'
 import Preview from './IndexPreview'
 import Transfer from './Transfer'
 export default {
   name: 'onlineDev-visualPortal',
-  components: { Form, Preview, Transfer },
+  components: { Form, Form1, Preview, Transfer },
   data() {
     return {
       list: [],
@@ -110,10 +132,12 @@ export default {
       total: 0,
       activeId: '',
       transferId: '',
+      dialogVisible: false,
       previewVisible: false,
       transferShow: false,
       listLoading: false,
       formVisible: false,
+      form1Visible: false,
       categoryList: []
     }
   },
@@ -204,16 +228,79 @@ export default {
       this.transferId = id
       this.transferShow = true
     },
-    addOrUpdateHandle(id) {
-      this.formVisible = true
-      this.$nextTick(() => {
-        this.$refs.Form.init(this.categoryList, id)
-      })
+    addOrUpdateHandle(type, id) {
+      this.dialogVisible = false
+      const key = type === 1 ? 'form1' : 'form'
+      const time = type === 1 && !id ? 300 : 0
+      setTimeout(() => {
+        this[key + 'Visible'] = true
+        this.$nextTick(() => {
+          this.$refs[key].init(this.categoryList, id)
+        })
+      }, time);
     },
     closeForm(isRefresh) {
       this.formVisible = false
+      if (isRefresh) this.initData()
+    },
+    closeForm1(isRefresh) {
+      this.form1Visible = false
       if (isRefresh) this.initData()
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.JNPF-dialog-add {
+  >>> .el-dialog__body {
+    padding: 50px 30px !important;
+  }
+}
+.add-main {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .add-item {
+    width: 255px;
+    height: 136px;
+    background: #eff9ff;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    padding-left: 20px;
+    &.add-item-sys {
+      background: #f1f5ff;
+      .add-icon {
+        background: #ccd9ff;
+        color: #537eff;
+      }
+    }
+    .add-icon {
+      width: 56px;
+      height: 56px;
+      margin-right: 10px;
+      background: #ceeaff;
+      border-radius: 10px;
+      color: #46adfe;
+      flex-shrink: 0;
+      font-size: 30px;
+      line-height: 56px;
+      text-align: center;
+    }
+    .add-txt {
+      height: 56px;
+      P {
+        line-height: 28px;
+      }
+      .add-title {
+        font-size: 18px;
+        font-weight: bold;
+      }
+      .add-desc {
+        color: #8d8989;
+        font-size: 12px;
+      }
+    }
+  }
+}
+</style>
