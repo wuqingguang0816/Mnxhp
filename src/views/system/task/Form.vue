d11f78576f217f7ac520ca9f53993a779c5ebbb4
zhm <zhuhuimin@yinmaisoft.com>
2022-03-17 17:50:47 +0800

    【优化-任务调度】代码优化

--- src/views/system/task/Form.vue
+++ src/views/system/task/Form.vue
@@ -29,7 +29,8 @@
                 <el-radio-group v-model="dataForm.executeType" @change="onExecuteTypeChange">
                   <el-radio label="1">Api数据</el-radio>
                   <el-radio label="2">SQL操作</el-radio>
-                  <el-radio label="3">本地任务(获取在程序中事先写好的本地方法)</el-radio>
+                  <el-radio label="3">本地任务(<i class="el-icon-warning-outline"></i>获取在程序中事先写好的本地方法)
+                  </el-radio>
                 </el-radio-group>
               </el-form-item>
             </el-col>
