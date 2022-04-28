e4c9537621cc6bf7abda33ebef36883be7b70f15
zengjunfeng <zengjunfeng@yinmaisoft.com>
2022-04-28 16:38:24 +0800

    【修复-系统管理-系统菜单】方案提交时增加数据库表字段

--- src/views/system/menu/components/dataAuthorize/DataSchemeForm.vue
+++ src/views/system/menu/components/dataAuthorize/DataSchemeForm.vue
@@ -266,6 +266,8 @@ export default {
             text += '【'
             for (let j = 0; j < e.groups.length; j++) {
               let ee = e.groups[j]
+              let item = this.fieldOptions.filter(o => o.id === ee.id)[0]
+                  ee.bindTable = item.bindTable
               if (!ee.field || !ee.id || !ee.op || !ee.value) {
                 this.$message.warning("过滤条件检查出格式错误")
                 conditionValid = false
