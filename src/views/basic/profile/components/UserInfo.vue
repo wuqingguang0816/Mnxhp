676feded17c5009bb4cd887be806988c0ba7c978
zhm <zhuhuimin@yinmaisoft.com>
2022-05-17 15:43:12 +0800

    【优化-个人信息】个人信息姓名必填

--- src/views/basic/profile/components/UserInfo.vue
+++ src/views/basic/profile/components/UserInfo.vue
@@ -32,10 +32,10 @@
         </el-col>
       </el-tab-pane>
       <el-tab-pane label="个人资料">
-        <el-form ref="dataForm" :model="form2" label-width="100px">
+        <el-form ref="dataForm" :model="form2" :rules="form2Rule" label-width="100px">
           <el-row>
             <el-col :span="12">
-              <el-form-item label="姓名">
+              <el-form-item label="姓名" prop="realName">
                 <el-input v-model="form2.realName" />
               </el-form-item>
             </el-col>
@@ -174,6 +174,11 @@ export default {
         urgentTelePhone: '',
         postalAddress: '',
       },
+      form2Rule: {
+        realName: [
+          { required: true, message: '姓名不能为空', trigger: 'blur' }
+        ]
+      },
       certificatesTypeOptions: [],
       educationOptions: [],
       genderOptions: [],
@@ -217,13 +222,16 @@ export default {
       }
     },
     dataFormSubmit() {
-      UpdateUser(this.form2).then(res => {
-        this.$message({
-          message: res.msg,
-          type: 'success',
-          duration: 1500
+      this.$refs['dataForm'].validate((valid) => {
+        if (!valid) return
+        UpdateUser(this.form2).then(res => {
+          this.$message({
+            message: res.msg,
+            type: 'success',
+            duration: 1500
+          })
+          this.$store.commit('user/SET_USERINFO_USERNAME', this.form2.realName)
         })
-        this.$store.commit('user/SET_USERINFO_USERNAME', this.form2.realName)
       })
     }
   }
