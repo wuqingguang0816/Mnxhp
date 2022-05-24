3257564ce977200bae46d2f30af941ac579e0dc9
zengjunfeng <zengjunfeng@yinmaisoft.com>
2022-05-24 10:07:39 +0800

    【优化-系统菜单】代码格式化

--- src/views/system/menu/components/connectForm/index.vue
+++ src/views/system/menu/components/connectForm/index.vue
@@ -1,66 +1,29 @@
 <template>
-  <el-dialog
-    title="数据连接"
-    :close-on-click-modal="false"
-    :close-on-press-escape="false"
-    :visible.sync="visible"
-    lock-scroll
-    class="JNPF-dialog JNPF-dialog_center"
-    width="630px"
-  >
-    <el-form
-      ref="dataForm"
-      :model="dataForm"
-      :rules="dataRule"
-      label-width="80px"
-      v-loading="formLoading"
-      class="menuForm"
-    >
+  <el-dialog title="数据连接" :close-on-click-modal="false" :close-on-press-escape="false"
+    :visible.sync="visible" lock-scroll class="JNPF-dialog JNPF-dialog_center" width="630px">
+    <el-form ref="dataForm" :model="dataForm" :rules="dataRule" label-width="80px"
+      v-loading="formLoading" class="menuForm">
       <el-form-item label="数据连接" prop="dbLinkId">
-        <el-select
-          v-model="dataForm.dbLinkId"
-          placeholder="请选择数据库"
-          @change="onDbChange"
-          clearable
-        >
-          <el-option-group
-            v-for="group in dbOptions"
-            :key="group.fullName"
-            :label="group.fullName"
-          >
-            <el-option
-              v-for="item in group.children"
-              :key="item.id"
-              :label="item.fullName"
-              :value="item.id"
-            />
+        <el-select v-model="dataForm.dbLinkId" placeholder="请选择数据库" @change="onDbChange" clearable>
+          <el-option-group v-for="group in dbOptions" :key="group.fullName" :label="group.fullName">
+            <el-option v-for="item in group.children" :key="item.id" :label="item.fullName"
+              :value="item.id" />
           </el-option-group>
         </el-select>
       </el-form-item>
       <el-form-item label="数据选择" prop="dataSelect" class="el-select">
         <div @click="jumpTable">
-          <el-input
-            placeholder="请选择数据表"
-            v-model="dataForm.dataSelect"
-            readonly
-            :validate-event="false"
-            @mouseenter.native="inputHovering = true"
-            @mouseleave.native="inputHovering = false"
-          >
+          <el-input placeholder="请选择数据表" v-model="dataForm.dataSelect" readonly
+            :validate-event="false" @mouseenter.native="inputHovering = true"
+            @mouseleave.native="inputHovering = false">
             <template slot="suffix">
-              <i
-                v-show="!showClose"
-                :class="[
+              <i v-show="!showClose" :class="[
                   'el-select__caret',
                   'el-input__icon',
                   'el-icon-arrow-up'
-                ]"
-              ></i>
-              <i
-                v-if="showClose"
-                class="el-select__caret el-input__icon el-icon-circle-close"
-                @click.stop="clear"
-              ></i>
+                ]"></i>
+              <i v-if="showClose" class="el-select__caret el-input__icon el-icon-circle-close"
+                @click.stop="clear"></i>
             </template>
           </el-input>
         </div>
@@ -71,16 +34,10 @@
         $t("common.cancelButton")
       }}</el-button>
       <el-button type="primary" :loading="btnLoading" @click="dataFormSubmit()">
-        {{ $t("common.confirmButton") }}</el-button
-      >
+        {{ $t("common.confirmButton") }}</el-button>
     </span>
-    <DataTableForm
-      :visible.sync="formVisible"
-      ref="tableForm"
-      @closeForm="closeForm"
-      :dbLinkId="dataForm.dbLinkId"
-      :dataSelect="dataForm.dataSelect"
-    />
+    <DataTableForm :visible.sync="formVisible" ref="tableForm" @closeForm="closeForm"
+      :dbLinkId="dataForm.dbLinkId" :dataSelect="dataForm.dataSelect" />
   </el-dialog>
 </template>
 
@@ -110,7 +67,7 @@ export default {
         ]
       },
       dbOptions: [],
-      type:0
+      type: 0
     };
   },
   computed: {
@@ -124,7 +81,7 @@ export default {
     }
   },
   methods: {
-    init(dbOptions,type) {
+    init(dbOptions, type) {
       this.type = type != undefined ? type : 1
       this.visible = true;
       this.dbOptions = dbOptions;
@@ -135,19 +92,19 @@ export default {
     dataFormSubmit() {
       this.$refs["dataForm"].validate(valid => {
         if (valid) {
-          getDataModelFields(this.dataForm.dbLinkId,this.dataForm.dataSelect,this.type)
+          getDataModelFields(this.dataForm.dbLinkId, this.dataForm.dataSelect, this.type)
             .then(res => {
-              this.$emit("refreshDataList", res.data.list,this.dataForm.dataSelect);
+              this.$emit("refreshDataList", res.data.list, this.dataForm.dataSelect);
               this.visible = false;
             })
-            .catch(() => {});
+            .catch(() => { });
         }
       });
     },
     jumpTable() {
       this.formVisible = true;
     },
-    onDbChange() {},
+    onDbChange() { },
     closeForm(table, row) {
       //   let data = JSON.parse(JSON.stringify(dbData));
       this.dataForm.dataSelect = table;
