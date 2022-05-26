b8c3923be6b304e59d40007a483ba4d721f45c34
zhm <zhuhuimin@yinmaisoft.com>
2022-05-26 14:35:29 +0800

    【优化-系统配置】系统版本改为只读

--- src/views/system/sysConfig/index.vue
+++ src/views/system/sysConfig/index.vue
@@ -21,8 +21,7 @@
             </el-col>
             <el-col :span="12">
               <el-form-item label="系统版本" prop="sysVersion">
-                <el-input v-model="baseForm.sysVersion" maxlength="8" clearable
-                  placeholder="系统版本" />
+                <el-input v-model="baseForm.sysVersion" maxlength="8" readonly placeholder="系统版本" />
               </el-form-item>
             </el-col>
             <el-col :span="12">
