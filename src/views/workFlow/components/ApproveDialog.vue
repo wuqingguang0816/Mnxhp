c06dc126fea0847008f5e85bb55c9d2a39353b65
zhm <zhuhuimin@yinmaisoft.com>
2022-06-23 16:48:32 +0800

    【优化-工作流程】审批弹窗样式优化

--- src/views/workFlow/components/ApproveDialog.vue
+++ src/views/workFlow/components/ApproveDialog.vue
@@ -2,7 +2,8 @@
   <el-dialog :title="eventType==='audit'?'审批通过':'审批拒绝'" :close-on-click-modal="false"
     :visible.sync="visible" class="JNPF-dialog JNPF-dialog_center" lock-scroll append-to-body
     width='600px'>
-    <el-form label-width="130px" ref="dataForm" :model="dataForm">
+    <el-form ref="dataForm" :model="dataForm" candidateList
+      :label-width="dataForm.candidateList.length||branchList.length?'130px':'80px'">
       <template v-if="eventType==='audit'">
         <el-form-item label="分支选择" prop="branchList" v-if="branchList.length"
           :rules="[{ required: true, message: `分支不能为空`, trigger: 'change' }]">
