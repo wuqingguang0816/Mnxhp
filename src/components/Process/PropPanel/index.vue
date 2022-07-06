82ff93450166f550312ba929dd21307a9b98980c
zhm <zhuhuimin@yinmaisoft.com>
2022-07-06 16:34:15 +0800

    【优化-流程设计】流程设计超时提醒优化

--- src/components/Process/PropPanel/index.vue
+++ src/components/Process/PropPanel/index.vue
@@ -923,7 +923,7 @@
                 <el-row :gutter="20">
                   <el-col :span="8">节点处理限定时长(时)</el-col>
                   <el-col :span="16">
-                    <el-input-number v-model="startForm.timeLimitConfig.duringDeal" :min="0"
+                    <el-input-number v-model="startForm.timeLimitConfig.duringDeal" :min="1"
                       :step="1"></el-input-number>
                   </el-col>
                 </el-row>
@@ -1699,7 +1699,7 @@
                 <el-row :gutter="20">
                   <el-col :span="8">节点处理限定时长(时)</el-col>
                   <el-col :span="16">
-                    <el-input-number v-model="approverForm.timeLimitConfig.duringDeal" :min="0"
+                    <el-input-number v-model="approverForm.timeLimitConfig.duringDeal" :min="1"
                       :step="1"></el-input-number>
                   </el-col>
                 </el-row>
