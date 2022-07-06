f5edcff9cee4f88de3b92df0dfebcb329e5748ac
shejinsheng <shejinsheng@yinmaisoft.com>
2022-07-06 10:37:48 +0800

    【优化-在线开发-功能设计】功能设计更改模式优化

--- src/views/generator/AddBox.vue
+++ src/views/generator/AddBox.vue
@@ -2,14 +2,14 @@
   <el-dialog :title="webType?'更改模式':'新建'" :close-on-click-modal="false" lock-scroll v-bind="$attrs"
     v-on="$listeners" class="JNPF-dialog JNPF-dialog_center" :width="webType?'310px':'580px'">
     <div class="add-main">
-      <div class="add-item add-item-form" @click="add(1)" v-if="webType==2||webType==3">
+      <div class="add-item add-item-form" @click="add(1)" v-if="webType!==1">
         <i class="add-icon el-icon-document"></i>
         <div class="add-txt">
           <p class="add-title">表单</p>
           <p class="add-desc">纯表单设计</p>
         </div>
       </div>
-      <div class="add-item add-item-list" @click="add(2)" v-if="webType==1">
+      <div class="add-item add-item-list" @click="add(2)" v-if="webType!==2&&webType!==3">
         <i class="add-icon icon-ym icon-ym-sysMenu"></i>
         <div class="add-txt">
           <p class="add-title">列表</p>
