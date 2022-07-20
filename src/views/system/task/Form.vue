bf3f10b3cd52235a519d79ce44eb8a29eb3d6193
zhm <zhuhuimin@yinmaisoft.com>
2022-07-20 10:23:01 +0800

    【优化-系统调度】代码优化

--- src/views/system/task/Form.vue
+++ src/views/system/task/Form.vue
@@ -53,7 +53,7 @@
                     @change="onFuncChange" />
                 </el-form-item>
               </el-col>
-              <el-col :span="24">
+              <el-col :span="24" v-if="dataForm.executeContent.parameter.length">
                 <el-form-item label="请求参数" prop="executeContent.parameter">
                   <el-row v-for="(item,i) in dataForm.executeContent.parameter" :key="i"
                     class="mb-10">
