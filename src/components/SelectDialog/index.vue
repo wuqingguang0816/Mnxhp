e652ff68e18dc3061b0138384495873570925182
zhm <zhuhuimin@yinmaisoft.com>
2022-08-11 14:53:23 +0800

    【优化-在线开发】子表选择关联弹窗代码优化

--- src/components/SelectDialog/index.vue
+++ src/components/SelectDialog/index.vue
@@ -2,8 +2,8 @@
   <div class="main">
     <template v-if="config.popupType==='dialog'">
       <el-dialog :title="config.popupTitle" :close-on-click-modal="false" :visible.sync="visible"
-        class="JNPF-dialog JNPF-dialog_center" lock-scroll append-to-body
-        :width='config.popupWidth'>
+        class="JNPF-dialog JNPF-dialog_center" lock-scroll append-to-body :width='config.popupWidth'
+        @close="$emit('close')">
         <el-row class="JNPF-common-search-box" :gutter="16">
           <el-form @submit.native.prevent>
             <el-col :span="10">
@@ -40,7 +40,8 @@
     </template>
     <template v-if="config.popupType==='drawer'">
       <el-drawer :title="config.popupTitle" :visible.sync="visible" :wrapperClosable="false"
-        ref="drawer" :size='config.popupWidth' append-to-body class="JNPF-common-drawer">
+        ref="drawer" :size='config.popupWidth' append-to-body class="JNPF-common-drawer"
+        @close="$emit('close')">
         <div class="JNPF-flex-main">
           <el-row class="JNPF-common-search-box" :gutter="16">
             <el-form @submit.native.prevent>
@@ -173,6 +174,7 @@ export default {
         }
         checked.push(item)
       }
+      this.$emit('close')
       this.$emit('select', checked)
     },
     handleSelectionChange(val) {
