8bdba207865b4f78d58a98e290c27963f1141c77
zengjunfeng <zengjunfeng@yinmaisoft.com>
2022-08-20 18:18:20 +0800

    【新增-系统管理-消息中心-账号配置】webhook配置

---
+++ src/views/msgCenter/accountConfig/webhookConfig/Form.vue
@@ -0,0 +1,152 @@
+<template>
+  <el-dialog :title="!dataForm.id ? '新建' : '编辑'" :close-on-click-modal="false"
+    :visible.sync="visible" class="JNPF-dialog JNPF-dialog_center" lock-scroll width="600px">
+    <el-form ref="dataForm" :model="dataForm" :rules="dataRule" v-loading="formLoading"
+      label-width="120px">
+      <el-form-item label="名称" prop="fullName">
+        <el-input v-model="dataForm.fullName" placeholder="输入名称" />
+      </el-form-item>
+      <el-form-item label="编码" prop="enCode">
+        <el-input v-model="dataForm.enCode" placeholder="业务编码" />
+      </el-form-item>
+      <el-form-item label="类型" prop="webhookType">
+        <el-select v-model="dataForm.webhookType" placeholder="请选择" clearable>
+          <el-option v-for="(item,index) in webhookList" :label="item.fullName" :value="item.enCode"
+            :key="index" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="WebHook地址" prop="webhookAddress">
+        <el-input v-model="dataForm.webhookAddress" placeholder="业务编码" />
+      </el-form-item>
+      <el-form-item label="认证类型" prop="approveType">
+        <el-select v-model="dataForm.approveType" placeholder="请选择" clearable>
+          <el-option v-for="(item,index) in approveTypeList" :label="item.fullName"
+            :value="item.enCode" :key="index" />
+        </el-select>
+      </el-form-item>
+      <template v-if="dataForm.approveType == 2">
+        <el-form-item label="Bearer令牌" prop="bearer">
+          <el-input v-model="dataForm.bearer" placeholder="Bearer令牌" />
+        </el-form-item>
+      </template>
+      <template v-if="dataForm.approveType == 3">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="dataForm.userName" placeholder="业务编码" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="dataForm.password" placeholder="业务编码" />
+        </el-form-item>
+      </template>
+      <el-form-item label="排序" prop="sortCode">
+        <el-input-number :min="0" :max="999999" v-model="dataForm.sortCode"
+          controls-position="right" />
+      </el-form-item>
+      <el-form-item label="状态" prop="enabledMark">
+        <el-switch v-model="dataForm.enabledMark" :active-value="1" :inactive-value="0" />
+      </el-form-item>
+      <el-form-item label="说明" prop="description">
+        <el-input v-model="dataForm.description" type="textarea" :rows="3" />
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">{{$t('common.cancelButton')}}</el-button>
+      <el-button type="primary" :loading="btnLoading" @click="dataFormSubmit()">
+        {{$t('common.confirmButton')}}</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import {
+  createConfig,
+  updateConfig,
+  getConfigDetail
+} from '@/api/msgCenter/accountConfig'
+export default {
+  data() {
+    return {
+      visible: false,
+      formLoading: false,
+      btnLoading: false,
+      dataForm: {
+        fullName: '',
+        enCode: '',
+        webhookType: '',
+        webhookAddress: "",
+        approveType: 1,
+        bearer: "",
+        userName: '',
+        password: '',
+        sortCode: 0,
+        enabledMark: 1,
+        description: ''
+      },
+      webhookList: [],
+      approveTypeList: [{ fullName: '无需认证', enCode: 1 }, { fullName: 'bearer令牌', enCode: 2 }, { fullName: '基本认证', enCode: 3 }],
+      dataRule: {
+        fullName: [
+          { required: true, message: '请输入业务名称', trigger: 'blur' },
+          { max: 50, message: '业务名称最多为50个字符！', trigger: 'blur' }
+        ],
+        enCode: [
+          { required: true, message: '请输入业务编码', trigger: 'blur' },
+          { max: 50, message: '业务编码最多为50个字符！', trigger: 'blur' }
+        ],
+        webhookType: [
+          { required: true, message: '请选择类型', trigger: 'change' }
+        ],
+        approveType: [
+          { required: true, message: '请选择认证类型', trigger: 'change' }
+        ],
+        webhookAddress: [{ required: true, message: '请输入业务编码', trigger: 'blur' }],
+        bearer: [{ required: true, message: '请输Bearer令牌', trigger: 'blur' }],
+        userName: [{ required: true, message: '请输用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    init(id, webhookList) {
+      this.visible = true
+      this.formLoading = true
+      this.dataForm.id = id || ''
+      this.webhookList = webhookList
+      this.$nextTick(() => {
+        this.$refs['dataForm'].resetFields()
+        if (this.dataForm.id) {
+          getConfigDetail(this.dataForm.id).then(res => {
+            this.dataForm = res.data
+          })
+        }
+      })
+      this.formLoading = false
+    },
+    dataFormSubmit() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.btnLoading = true
+          let query = {
+            ...this.dataForm,
+            type: 7
+          }
+          const formMethod = query.id ? updateConfig : createConfig
+          formMethod(query).then(res => {
+            this.$message({
+              message: res.msg,
+              type: 'success',
+              duration: 1500,
+              onClose: () => {
+                this.visible = false
+                this.btnLoading = false
+                this.$emit('refreshDataList')
+              }
+            })
+          }).catch(() => {
+            this.btnLoading = false
+          })
+        }
+      })
+    }
+  }
+}
+</script>
