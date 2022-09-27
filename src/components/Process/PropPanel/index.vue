b244f9a7f22ad1ba617bf3bae251a32d4580ed13
zhm <zhuhuimin@yinmaisoft.com>
2022-09-27 13:55:17 +0800

    【新增-工作流程-流程设计】候选人默认所有人

--- src/components/Process/PropPanel/index.vue
+++ src/components/Process/PropPanel/index.vue
@@ -3038,13 +3038,6 @@ export default {
         })
         return
       }
-      if (assigneeType == 7 && !this.approverForm.approvers.length && !this.approverForm.approverPos.length && !this.approverForm.approverRole.length) {
-        this.$message({
-          message: '请设置候选人员',
-          type: 'error',
-        })
-        return
-      }
       if (assigneeType == 9 && !this.approverForm.getUserUrl) {
         this.$message({
           message: '请输入接口路径',
