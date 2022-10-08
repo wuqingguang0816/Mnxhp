5034e0ed36fda29b98971467f7b8004a577d4059
zhm <zhuhuimin@yinmaisoft.com>
2022-10-08 14:08:38 +0800

    【优化-工作流程-表单设计】表单设计预览代码优化
    【新增-工作流程-表单设计】新增表单设计

--- src/views/workFlow/formDesign/Preview.vue
+++ src/views/workFlow/formDesign/Preview.vue
@@ -2,7 +2,7 @@
   <transition name="el-zoom-in-center">
     <div class="JNPF-preview-main flow-form-main">
       <div class="JNPF-common-page-header">
-        <el-page-header @back="goBack" :content="'表单预览【'+setting.fullName+'】'" />
+        <el-page-header @back="goBack" :content="'预览表单【'+setting.fullName+'】'" />
         <div class="options">
           <el-button @click="goBack()">{{$t('common.cancelButton')}}</el-button>
         </div>
@@ -30,20 +30,13 @@ export default {
     init(data) {
       this.setting = data
       getFormInfo(data.formId).then(res => {
-        if (!res.data || !res.data.draftJson) return
         const dataSource = data.dataSource === "propertyJson" ? "propertyJson" : "draftJson"
+        if (!res.data || !res.data[dataSource]) return
         data.formConf = res.data[dataSource]
         data.type = res.data.type
         data.formOperates = []
-        if (data.formType == 1) {
-          if (res.data.formUrl) {
-            this.currentView = (resolve) => require([`@/views/${res.data.formUrl}`], resolve)
-          } else {
-            this.currentView = (resolve) => require([`@/views/workFlow/workFlowForm/${data.enCode}`], resolve)
-          }
-        } else {
-          this.currentView = (resolve) => require([`@/views/workFlow/workFlowForm/dynamicForm`], resolve)
-        }
+        const formUrl = data.formType == 2 ? 'workFlow/workFlowForm/dynamicForm' : res.data.urlAddress ? res.data.urlAddress : `workFlow/workFlowForm/${data.enCode}`
+        this.currentView = (resolve) => require([`@/views/${formUrl}`], resolve)
         setTimeout(() => {
           this.$nextTick(() => {
             this.$refs.form && this.$refs.form.init(data)
