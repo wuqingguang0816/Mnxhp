43f9351168095f546d1322d251b38b416efb702c
fan <912374021@qq.com>
2022-12-07 13:58:35 +0800

    【修复-在线开发】功能带流程预览草稿问题

--- src/views/workFlow/components/FlowBox.vue
+++ src/views/workFlow/components/FlowBox.vue
@@ -407,7 +407,7 @@ export default {
         this.flowTaskOperatorRecordList = this.flowTaskOperatorRecordList.reverse()
         this.properties = res.data.approversProperties || {}
         this.endTime = this.flowTaskInfo.completion == 100 ? this.flowTaskInfo.endTime : 0
-        data.formConf = this.flowFormInfo.propertyJson
+        data.formConf = data.isPreview ? data.formConf : this.flowFormInfo.propertyJson
         if (data.opType != 1 && data.opType != '-1') data.readonly = true
         data.formOperates = res.data.formOperates || []
         if (data.opType == 0) {
