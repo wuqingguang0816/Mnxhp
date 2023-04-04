937f5ac7c79f91fa1259e30de7b9bfd70dca8d55
zengjunfeng <zengjunfeng@yinmaisoft.com>
2023-04-04 17:35:51 +0800

    【优化-全局】只在pc模式下展示只读

--- src/components/Generator/index/RightComponents/ComInput.vue
+++ src/components/Generator/index/RightComponents/ComInput.vue
@@ -47,7 +47,7 @@
     <el-form-item label="是否密码">
       <el-switch v-model="activeData['show-password']" />
     </el-form-item>
-    <el-form-item label="是否只读">
+    <el-form-item label="是否只读" v-show="showType==='pc'">
       <el-switch v-model="activeData.readonly" />
     </el-form-item>
     <el-form-item label="是否禁用">
