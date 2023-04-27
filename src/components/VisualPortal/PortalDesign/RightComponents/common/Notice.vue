6cedcdc852f45b6af99de50b1879eee1ed7e2b79
shejinsheng <shejinsheng@yinmaisoft.com>
2023-04-27 17:32:29 +0800

    【修复-门户设计】显示条数

--- src/components/VisualPortal/PortalDesign/RightComponents/common/Notice.vue
+++ src/components/VisualPortal/PortalDesign/RightComponents/common/Notice.vue
@@ -115,8 +115,8 @@
         </el-form-item>
         <Refresh :refresh="activeData.refresh" />
         <jnpf-form-tip-item label="显示条数" tipLabel="置空时，则显示全部数据">
-          <el-input-number v-model="activeData.option.tableCount" controls-position="right" :min="1"
-            :max="99999" @change="renderKeyChange" />
+          <el-input-number v-model="activeData.option.noticeCount" controls-position="right"
+            :min="1" :max="99999" @change="renderKeyChange" />
         </jnpf-form-tip-item>
       </template>
       <NoticeColumnData v-if="columnVisible" ref="noticeColumnData" :showType='showType'
