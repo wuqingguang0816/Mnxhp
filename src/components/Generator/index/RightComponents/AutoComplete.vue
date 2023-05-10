bd085527c3c238f5e4d060f7cb2050ca31dee6b9
shejinsheng <shejinsheng@yinmaisoft.com>
2023-05-10 18:11:12 +0800

    Update AutoComplete.vue

--- src/components/Generator/index/RightComponents/AutoComplete.vue
+++ src/components/Generator/index/RightComponents/AutoComplete.vue
@@ -38,7 +38,7 @@
       </el-table-column>
     </el-table>
     <jnpf-form-tip-item label="显示条数" tipLabel="置空时，则显示全部数据">
-      <el-input-number v-model="activeData.total" placeholder="默认值" :min="0" :max="50"
+      <el-input-number v-model="activeData.total" placeholder="默认值" :min="1" :max="50"
         :precision="activeData.precision" controls-position="right" />
     </jnpf-form-tip-item>
     <el-form-item label="能否清空">
