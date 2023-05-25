88a929ab16e73fe232a214d3b63d53c3f9a7e1bd
zengjunfeng <zengjunfeng@yinmaisoft.com>
2023-05-25 12:03:10 +0800

    【优化-消息】消息详情

--- src/views/basic/messageRecord/Form.vue
+++ src/views/basic/messageRecord/Form.vue
@@ -9,7 +9,7 @@
           <span>{{info.releaseTime|toDate()}}</span><span>{{info.releaseUser}}</span>
         </div>
       </div>
-      <p style="padding: 20px 0 10px;color: #303133;">{{ info.excerpt }}</p>
+      <p class="excerpt">{{ info.excerpt }}</p>
       <div class="main" v-html="info.bodyText"></div>
       <div class="file-list" v-if="files.length">
         <JNPF-UploadFz v-model="files" disabled detailed />
