c9148298017412dc608ae90cd11d650c24ff2ea9
fan <912374021@qq.com>
2023-05-29 10:41:26 +0800

    【修复-数据接口】提交加载中样式

--- src/views/systemData/dataInterface/Form.vue
+++ src/views/systemData/dataInterface/Form.vue
@@ -729,13 +729,16 @@ export default {
       this.$refs.SQLEditorRef.insert('{' + item.field + '}')
     },
     dataFormSubmit() {
-      this.btnLoading = true
       if (this.dataForm.dataType !== 2) {
+        this.btnLoading = true
         this.dataForm.dataProcessing = this.text
         this.handleSubmit()
       } else {
         this.$refs['dataForm'].validate(valid => {
-          if (valid) this.handleSubmit()
+          if (valid) {
+            this.btnLoading = true
+            this.handleSubmit()
+          }
         })
       }
     },
