314881fe5f720ad8b7c93422152aa7ae8fdba89d
shejinsheng <shejinsheng@yinmaisoft.com>
2023-07-19 18:00:33 +0800

    【修复-全局】端口

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,4 +1,4 @@
 module.exports = {
   // 开发环境接口配置
-  APIURl: 'http://192.168.0.242:30000'
+  APIURl: 'http://localhost:30000'
 }
--- src/utils/define.js
+++ src/utils/define.js
@@ -10,9 +10,9 @@ module.exports = {
   // 大屏应用前端路径
   dataV: process.env.NODE_ENV === 'development' ? 'http://localhost:8100/DataV' : window.location.origin + '/DataV',
   // 数据报表接口-java boot
-  // reportServer: process.env.NODE_ENV === 'development' ? 'http://localhost:30007' : process.env.VUE_APP_BASE_API + '/ReportServer',
+  reportServer: process.env.NODE_ENV === 'development' ? 'http://localhost:30007' : process.env.VUE_APP_BASE_API + '/ReportServer',
   // 数据报表接口-java cloud
-  reportServer: process.env.NODE_ENV === 'development' ? 'http://192.168.0.242:30000' : process.env.VUE_APP_BASE_API,
+  //reportServer: process.env.NODE_ENV === 'development' ? 'http://localhost:30000' : process.env.VUE_APP_BASE_API,
   // 报表前端
   report: process.env.NODE_ENV === 'development' ? 'http://localhost:8200' : process.env.VUE_APP_BASE_API + '/Report'
 }
