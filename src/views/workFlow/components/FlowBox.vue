a7eb4acefba5eee3f9d9102a00212e6961842ae0
lingen <1762861715@qq.com>
2023-08-01 09:29:11 +0800

    【修复-流程打印】打印id错误

--- src/views/workFlow/components/FlowBox.vue
+++ src/views/workFlow/components/FlowBox.vue
@@ -639,7 +639,7 @@ export default {
     printDialog() {
       this.printDialogVisible = true
       this.$nextTick(() => {
-        this.$refs.printDialog.init(this.properties.printId)
+        this.$refs.printDialog.init(JSON.parse(this.properties.printId))     
       })
     },
     handleMore(e) {
