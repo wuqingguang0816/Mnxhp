fbd605622f152402245ba7216b4af416afa1067a
shejinsheng <shejinsheng@yinmaisoft.com>
2023-08-09 09:35:08 +0800

    【修复-流程审批】退回节点

--- src/views/workFlow/components/FlowBox.vue
+++ src/views/workFlow/components/FlowBox.vue
@@ -712,7 +712,7 @@ export default {
           RejectList(this.setting.taskId).then(res => {
             this.showReject = res.data.isLastAppro
             this.rejectList = res.data.list || []
-            this.candidateForm.rejectStep = this.rejectList[0].nodeCode
+            this.candidateForm.rejectStep = this.rejectList.length && this.rejectList[0].nodeCode || ''
             if (!this.properties.hasSign && !this.properties.hasOpinion && !this.properties.isCustomCopy && !this.showReject) {
               this.$confirm('此操作将退回该审批单，是否继续？', '提示', {
                 type: 'warning'
